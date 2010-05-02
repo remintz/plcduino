4ca5f47e9925d117c22cc9d2b2f794a9f6790d1a
remintz <remintz@localhost>
2010-05-02 21:51:15 +0000

    * Funcionou TON * Funcionou TP

--- PLCDuinoRun/src/timer.cpp
+++ PLCDuinoRun/src/timer.cpp
@@ -1,3 +1,4 @@
+#include <WProgram.h>
 #include "MsTimer2.h"
 
 #define TIMER_CICLE		30000
@@ -5,6 +6,7 @@
 int timerCount;
 
 void tickTimer() {
+//	Serial.print("T");
 	timerCount++;
 	if (timerCount >= TIMER_CICLE) {
 		timerCount = 0;
@@ -16,15 +18,20 @@ int getTimer() {
 }
 
 int diffTimer(int since) {
+//	Serial.print("diffTimer: since=");
+//	Serial.print(since, DEC);
+//	Serial.print("Cnt=");
+//	Serial.println(timerCount,DEC);
 	int t;
 	t = timerCount;
-	if (t > since)
+	if (t >= since)
 		return (t - since);
 	else
 		return (t + TIMER_CICLE - since);
 }
 
 void iniTimer() {
+	Serial.println("iniTimer");
 	timerCount = 0;
 	MsTimer2::set(1000, tickTimer);
 	MsTimer2::start();
