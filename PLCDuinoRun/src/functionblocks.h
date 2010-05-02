1441e869a7e8ded008567259dd057c40ebf3bd5c
remintz <remintz@localhost>
2010-05-02 22:13:31 +0000

    TOF functionando

--- PLCDuinoRun/src/functionblocks.h
+++ PLCDuinoRun/src/functionblocks.h
@@ -4,6 +4,7 @@
 void SRScan(unsigned char  *p_in, unsigned char  *p_out);
 void RSScan(unsigned char  *p_in, unsigned char  *p_out);
 void TONScan(unsigned char  *p_in, unsigned char  *p_out, int p_DeltaTime);
+void TOFScan(unsigned char  *p_in, unsigned char  *p_out, int p_DeltaTime);
 void TPScan(unsigned char *p_in, unsigned char *p_out, int p_DeltaTime);
 
 
